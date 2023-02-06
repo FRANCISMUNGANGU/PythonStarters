0eaf25a44140281444d62df8b2e732d9edb018dd
FRANCISMUNGANGU <francismungangu@gmail.com>
2023-02-06 17:10:59 +0300

    used python

---
+++ main.py
@@ -0,0 +1,36 @@
+greeting = "Hello world"
+print(greeting)
+miles = 1000.0
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(int(miles))
+
+xy = '5'
+a = '5'
+c = int(xy) + int(a)
+
+print(c)
+print(type(xy))
+
+name1 = "Francis"
+Name1 = "Mary"
+print(name1)
+
+fruit1, fruit2, fruit3 = 'orange', 'banana', 'cherry'
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+fruit4 = fruit5 = fruit6 = 'Apple'
+print(fruit4)
+print(fruit5)
+print(fruit6)
+
+fruits = ["banana", "apple", "cherry"]
+f1,f2,f3 = fruits
+print(f1)
+print(f2)
+print(f3)
